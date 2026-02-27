1bb2c696c9a0a0c5f60d892421bb1557894cfd00
ahmed elbardawell <brdweelahmed@gmail.com>
2026-02-27 02:04:58 +0200

    Fix incorrect model name CommunityPost to Post in web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use App\Http\Controllers\LanguageController;
 Route::get('/lang/{locale}', [LanguageController::class, 'switch'])->name('lang.switch');
 
 Route::get('/', function () {
-    $posts = \App\Models\CommunityPost::with('user')
+    $posts = \App\Models\Post::with('user')
         ->withCount(['likes', 'comments'])
         ->latest()
         ->take(3)
@@ -139,7 +139,7 @@ Route::prefix('preview')->name('preview.')->middleware(\App\Http\Middleware\Prev
     })->name('community.index');
     
     Route::get('/community/post', function () { 
-        $dummyPost = new \App\Models\CommunityPost();
+        $dummyPost = new \App\Models\Post();
         $dummyPost->id = 1;
         $dummyPost->content = 'Sample content';
         $dummyPost->user_id = 1;
