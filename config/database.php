85e5e8ee9592b5b794cfbab431b82f591dd14674
ahmed elbardawell <brdweelahmed@gmail.com>
2026-02-27 15:00:08 +0200

    Robustness fix: trim DATABASE_URL as well

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => trim(env('DATABASE_URL')),
             'host' => trim(env('DB_HOST', '127.0.0.1')),
             'port' => trim(env('DB_PORT', '3306')),
             'database' => trim(env('DB_DATABASE', 'forge')),
