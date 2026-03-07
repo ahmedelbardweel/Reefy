92a5c7b7fc8f4c2a5eb835749b25f04a30b90058
ahmed elbardawell <brdweelahmed@gmail.com>
2026-03-07 18:29:35 +0200

    remove all project files

--- resources/views/components/primary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-5 py-3 bg-green-700 border border-transparent font-bold text-xs text-white uppercase tracking-widest hover:bg-green-800 focus:bg-green-800 active:bg-green-900 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
